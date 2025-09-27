c005a492268c93ab8a85b7a0a2b6fa7865f07a0e
Jay Kanjariya <jaykanjariya009@gmail.com>
2025-09-27 19:17:49 +0530

    blog app

--- backend/db/connectDb.js
+++ backend/db/connectDb.js
@@ -1,23 +1,24 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
 const connectdb = async () => {
-    try {
-        await mongoose.connect(
-            process.env.MONGO_URI || 'mongodb+srv://jaykanjariya009:Jay%402004@cluster0.2wwtnns.mongodb.net/',
-            {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-            }
-        );
+  try {
+    await mongoose.connect(
+      process.env.MONGO_URI ||
+        "mongodb+srv://jaykanjariya009_db_user:nS6t5pkMJltnDRQw@cluster0.2c5h49r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
 
-        console.log("MongoDB Connected Successfully");
-    } catch (error) {
-        console.log("Error connecting to DB", error);
-        process.exit(1);
-    }
+    console.log("MongoDB Connected Successfully");
+  } catch (error) {
+    console.log("Error connecting to DB", error);
+    process.exit(1);
+  }
 };
 
 module.exports = connectdb;
