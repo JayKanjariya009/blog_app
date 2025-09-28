e2deaaa1097386bbedd0960a27a6683884b2776f
Jay Kanjariya <jaykanjariya009@gmail.com>
2025-09-28 18:01:35 +0530

    api change

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://blog-app-hh3f.onrender.com//api';
+const API_URL = 'https://blog-app-hh3f.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
