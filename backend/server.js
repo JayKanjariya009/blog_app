308681ead526951bdb0307f2cfed53f1a7bd6071
Jay Kanjariya <jaykanjariya009@gmail.com>
2025-09-28 19:41:44 +0530

    some changes in backend 08

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ app.use(
 const authRoutes = require("./routes/authRoutes");
 const blogRoutes = require("./routes/blogRoutes");
 const commentRoutes = require("./routes/commentRoutes");
-const { getImage } = require("./controllers/imageController");
+const { getImage } = require("./controllers/imagecontroller");
 
 app.use("/api/auth", authRoutes);
 app.use("/api/blogs", blogRoutes);
